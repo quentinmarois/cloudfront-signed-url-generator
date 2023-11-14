7359880c3d84023cab9d57bfde5b8789092befd5
Quentin Marois <quentin.marois@efrei.net>
2023-11-14 20:14:23 +0100

    feat: Check if the remote file exists

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { getSignedUrl } = require("@aws-sdk/cloudfront-signer");
 const { CloudFront } = require("@aws-sdk/client-cloudfront");
+const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3");
 require("dotenv").config();
 
 const ACCESS_KEY_ID = process.env.ACCESS_KEY_ID;
@@ -8,12 +9,13 @@ const REGION = process.env.REGION;
 const CF_ENDPOINT = process.env.CF_ENDPOINT;
 const CF_KEY_PAIR_ID = process.env.CF_KEY_PAIR_ID;
 const CF_PRIVATE_KEY = process.env.CF_PRIVATE_KEY;
+const BUCKET_NAME = process.env.BUCKET_NAME;
 
-async function generateSignedUrl () {
-  const file = "test.txt";
-  const timeout = 60 * 10; // 10 minutes
+const file = "test.txt";
+const timeout = 60 * 10; // 10 minutes
 
-  const cfClient = new CloudFront({
+async function doesFileExist (fileKey) {
+  const s3 = new S3Client({
     region: REGION,
     credentials: {
       accessKeyId: ACCESS_KEY_ID,
@@ -21,14 +23,50 @@ async function generateSignedUrl () {
     },
   });
 
-  const signedUrl = await getSignedUrl({
-    cfClient,
-    url: `${CF_ENDPOINT}/${file}`,
-    dateLessThan: new Date(Date.now() + timeout * 1000),
-    keyPairId: CF_KEY_PAIR_ID,
-    privateKey: CF_PRIVATE_KEY,
-  });
-  console.log(signedUrl);
+  const params = {
+    Bucket: BUCKET_NAME,
+    Key: fileKey,
+  };
+
+  try {
+    await s3.send(new GetObjectCommand(params));
+    return true;
+  } catch (error) {
+    if (error.name === 'NoSuchKey') {
+      return false;
+    }
+  }
+}
+
+async function generateSignedUrl () {
+  try {
+    const fileExist = await doesFileExist(file);
+    if (fileExist) {
+      const cfClient = new CloudFront({
+        region: REGION,
+        credentials: {
+          accessKeyId: ACCESS_KEY_ID,
+          secretAccessKey: SECRET_ACCESS_KEY,
+        },
+      });
+
+      const signedUrl = getSignedUrl({
+        cfClient,
+        url: `${CF_ENDPOINT}/${file}`,
+        dateLessThan: new Date(Date.now() + timeout * 1000),
+        keyPairId: CF_KEY_PAIR_ID,
+        privateKey: CF_PRIVATE_KEY,
+      });
+
+      console.log('Signed URL: ', signedUrl);
+
+      process.exit(0);
+    } else {
+      console.log('File does not exist');
+    }
+  } catch (error) {
+    console.error('Error generating signed URL: ', error.message || error);
+  }
 }
 
 generateSignedUrl();
